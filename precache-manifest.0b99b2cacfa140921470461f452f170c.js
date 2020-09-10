f0f49e6fd0b5aeb9dded5e95fc93d8e99245c8fe
TylerLeung <leung.tyler008@gmail.com>
2020-09-10 02:27:34 -0400

    Updates

---
+++ precache-manifest.0b99b2cacfa140921470461f452f170c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f426efd74418b2eda18bb64571ac53",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "927bc393c06f5e089d3a",
+    "url": "/todoapp/static/css/main.455105d8.chunk.css"
+  },
+  {
+    "revision": "8d21629c41174ceec1c8",
+    "url": "/todoapp/static/js/2.44b8750f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.44b8750f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "927bc393c06f5e089d3a",
+    "url": "/todoapp/static/js/main.f30148cb.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
